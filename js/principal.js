e50688e0db595f91edb03ad50e2c3a06b8abb199
ruvism <ruvila.m@gmail.com>
2019-03-06 23:31:28 -0300

    preventDefault

--- js/principal.js
+++ js/principal.js
@@ -41,3 +41,8 @@ for( var i = 0; i< pacientes.length; i++){
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+ event.preventDefault()   
+})
